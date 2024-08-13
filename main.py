8169451c96ead0623af5fe0a91131ab6c01ad72c
arafath756 <84910650+arafath756@users.noreply.github.com>
2024-08-13 21:01:20 +0530

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import mysql.connector
 import streamlit as st
 import pandas as pd
 import datetime as dt 
+from io import StringIO
 # streamlit run main.py
 
 # establish connection to mysql server
@@ -14,6 +15,15 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 print("connection establishment")
 
+def load_csv(uploaded_file):
+    try:
+        # Read CSV file into DataFrame
+        df = pd.read_csv(uploaded_file)
+        return df
+    except Exception as e:
+        st.error(f"Error reading the CSV file: {e}")
+        return None
+    
 # create streamlit app
 def main():
     st.title("Student registration form");
@@ -21,6 +31,26 @@ def main():
     # display option for CRUD operations
     option = st.selectbox("select an operation",("create","read","update","delete"))
     if option == "create":
+        st.subheader("Upload and Add CSV Data")
+        uploaded_file = st.file_uploader("Choose a CSV file", type='csv')
+        
+        if uploaded_file is not None:
+            df = load_csv(uploaded_file)
+            if df is not None:
+                st.write("Data preview:", df.head())
+
+                if st.button("Upload CSV to Database"):
+                    # Ensure all necessary columns are present in the DataFrame
+                    required_columns = ['name', 'fname', 'gmail', 'cnumber', 'dob']
+                    if all(col in df.columns for col in required_columns):
+                        for i, row in df.iterrows():
+                            sql = "INSERT INTO users (name, fname, gmail, cnumber, dob) VALUES (%s, %s, %s, %s, %s)"
+                            mycursor.execute(sql, tuple(row[required_columns]))
+                        mydb.commit()
+                        st.success("CSV Data Uploaded Successfully!")
+                    else:
+                        st.error("CSV file must contain the following columns: ")
+
         st.subheader("create record")
         name = st.text_input("enter your name")
         fname = st.text_input("enter your father name")
@@ -73,4 +103,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
